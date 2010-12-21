2fc27dfd3fc76e648555d9cb4cd88a19816cebdb
Alex Boisvert <alex.boisvert@gmail.com>
2010-12-20 23:27:19 -0800

    Add sample.png

--- high-pass-filter.sh
+++ high-pass-filter.sh
@@ -3,7 +3,9 @@ series=$1
 alpha=$2
 scala HighPassFilter.scala $series $alpha
 gnuplot <<END
-  plot [0:500] "$series" using 1 title "original" smooth csplines, \
+  set terminal png
+  set output "sample.png"
+  plot [0:2000] "$series" using 1 title "original" smooth csplines, \
     "$series.out" using 1 title "high pass filter" smooth csplines
   pause 30
 END
